d06921eed0717d890091b1b67407e735bb40a0e3
theoboldalex <theoboldalex@gmail.com>
2025-01-11 23:28:19 +0000

    comments ready for reading file

--- hello.asm
+++ hello.asm
@@ -4,29 +4,32 @@
 .section .data
 filename:
 	.asciz "content.txt"
+
 message:
 	.asciz "Welcome to Shat! The rubbish version of Cat I wrote in x86 asm\n\n"
+	
 message_size = . - message
 
 .section .text
 _start:
-	// sys_open call
+	// sys_open: open the file (currently hard coded)
 	mov rax, 2
 	lea rdi, [filename]
 	mov rsi, 0
 	mov rdx, 0
 	syscall
 
+	// was the sys_open call successful, if not, handle error
 	cmp rax, 0
 	jl error
 
 	// store the file descriptor
 	mov rdi, rax
 
-	// read file into buffer
-
+	// read file into buffer and get its length in bytes
 	
-	// write a message to stdout
+	
+	// write a header message to stdout
 	mov rax, 1
 	mov rdi, 1
 	lea rsi, [message]
